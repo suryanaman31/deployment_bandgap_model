fc7bb8534a638ca7ca218df6fa390798abd79af5
suryanaman31 <61493626+suryanaman31@users.noreply.github.com>
2021-06-10 00:38:14 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -10,6 +10,5 @@ app.register_blueprint(api_routes)
 
 app.config["JSON_SORT_KEYS"] = False
 
-# if __name__  == '__main__':
-#     app.debug = True
-#     app.run(host="0.0.0.0",port=5777)
+if __name__  == '__main__':
+     app.run(debug=True)
